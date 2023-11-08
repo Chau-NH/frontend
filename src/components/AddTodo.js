47c93eb9dc4eac47f48df67cea8aae16f7ef2a98
Chau Nguyen <chaunguyenh@VNNOT01842.HarveyNash.vn.local>
2023-11-08 13:24:16 +0700

    Rollout B/G

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -25,7 +25,7 @@ export default class AddTodo extends React.Component {
           className="form-control"
         />
         <button className="btn btn-primary" type="submit">
-          Add Todo 3
+          Add Todo 4
         </button>
       </form>
     );
